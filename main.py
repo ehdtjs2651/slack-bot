72031765917b7ecb8cbf00ca5ea061f4a6d608a9
ehdtjs2651 <81279713+ehdtjs2651@users.noreply.github.com>
2021-11-10 01:54:07 +0900

    main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, Response
 import os
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
+import ssl
 
 app = Flask(__name__)
 
@@ -13,6 +14,8 @@ def db():
 
 
 if __name__ == '__main__':
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+    ssl_context.load_cert_chain(certfile='/etc/letsencrypt/live/cyberclass.ga/cert.pem', keyfile='/etc/letsencrypt/live/cyberclass.ga/privkey.pem')
     client = WebClient(token="")
     port = 10300
     app.run(host='0.0.0.0', port=port, debug=True)
